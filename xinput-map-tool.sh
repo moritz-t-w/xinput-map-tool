ea602744a8f50633f47c32acd20b882371d8290e
Moritz Tim W <90388353+moritz-t-w@users.noreply.github.com>
2023-09-06 17:22:18 +0200

    improve output clarity

--- xinput-map-tool.sh
+++ xinput-map-tool.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Please unplug the relevant devices and press Enter to continue..."
+echo "Please unplug the relevant devices and press Enter..."
 read
 
 initial_xinput=$(xinput --list)
 initial_xrandr=$(xrandr)
 
-echo "Please plug in the relevant devices and press Enter to continue..."
+echo "Please plug in the relevant devices again and press Enter..."
 read
 
 final_xinput=$(xinput --list)
@@ -15,4 +15,5 @@ final_xrandr=$(xrandr)
 output_device=$(diff <(echo "$initial_xrandr") <(echo "$final_xrandr") | grep -oP '(?<=\> )[^ ]+(?= connected)')
 input_device=$(diff <(echo "$initial_xinput") <(echo "$final_xinput") | grep -oP '(?<=id=)[0-9]+(?=\t)')
 
+echo "Mapping input \"$input_device\" to output \"$output_device\"..."
 xinput map-to-output $input_device $output_device
